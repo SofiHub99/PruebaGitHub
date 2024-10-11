68e420e78ce04881631a5ba460fedefabac0cae3
2024-25DAM1 <2024-25DAM1@DESKTOP-S4VIGVJ>
2024-10-11 17:20:36 +0200

    Mi commit

---
+++ src/main/java/com/mycompany/pruebagithub/PruebaGitHub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebagithub;
+
+/**
+ *
+ * @author 2024-25DAM1
+ */
+public class PruebaGitHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
